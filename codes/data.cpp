67aef7c02658552293f3f947e82e1db54ea8d911
AdisheshBalaji <adishesh.balaji@gmail.com>
2024-10-29 13:03:41 +0530

    first commit

---
+++ codes/data.cpp
@@ -0,0 +1,11 @@
+#include <arduino.h>
+void setup(){
+      Serial.begin(9600)
+      }
+void loop(){
+      int sensorValue = analogRead(A0);
+      double voltage = (3.3*sensorValue/666);
+      Serial.println(voltage);
+      delay(1000);
+}
+      
